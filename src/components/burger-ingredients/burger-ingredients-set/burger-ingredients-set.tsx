9fbd075e011fb0a8b683328cea1fb46a193b35c1
Petruzzio <novikov.mailbox@gmail.com>
2023-08-24 14:23:04 +0300

    fixed BurgerIngredientsSet PropTypes

--- src/components/burger-ingredients/burger-ingredients-set/burger-ingredients-set.tsx
+++ src/components/burger-ingredients/burger-ingredients-set/burger-ingredients-set.tsx
@@ -23,20 +23,21 @@ function BurgerIngredientsSet(props) {
 
 BurgerIngredientsSet.propTypes = {
 	setName: PropTypes.string,
-	data: PropTypes.shape({
-		_id: PropTypes.string,
-		name: PropTypes.string,
-		type: PropTypes.string,
-		proteins: PropTypes.number,
-		fat: PropTypes.number,
-		carbohydrates: PropTypes.number,
-		calories: PropTypes.number,
-		price: PropTypes.number,
-		image: PropTypes.string,
-		image_mobile: PropTypes.string,
-		image_large: PropTypes.string,
-		__v: PropTypes.number
-	})
+	data: PropTypes.arrayOf(
+		PropTypes.shape({
+			_id: PropTypes.string,
+			name: PropTypes.string,
+			type: PropTypes.string,
+			proteins: PropTypes.number,
+			fat: PropTypes.number,
+			carbohydrates: PropTypes.number,
+			calories: PropTypes.number,
+			price: PropTypes.number,
+			image: PropTypes.string,
+			image_mobile: PropTypes.string,
+			image_large: PropTypes.string,
+			__v: PropTypes.number
+	}))
 }
 
 export default BurgerIngredientsSet;
